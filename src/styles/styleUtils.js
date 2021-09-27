b0b77987175b7236a4d9861c7029faf0e337f9d1
osemudiamen Elebesunu <eronscode@gmail.com>
2021-09-27 03:56:06 +0100

    style: adds default styling configurations

---
+++ src/styles/styleUtils.js
@@ -0,0 +1,50 @@
+import { css } from "styled-components";
+
+export const color = {
+  white: "#ffffff",
+  black: "#111111",
+  darkGrey: "#555456",
+  lightGrey: "#D3D3D3",
+  blue: "#0085FF",
+  lightBlue: "#B2DAFF",
+  red: "#FF0000",
+  yellow: "#FFD600",
+  green: "#00CC2D",
+};
+
+export const font = {
+  regular: 'font-family: "Roboto-Regular"; font-weight: 500;',
+  bold: 'font-family: "Roboto-Regular"; font-weight: 400;',
+  size: (size) => `font-size: ${size}px;`,
+};
+
+export const mixins = {
+  truncateText: css`
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  `,
+  clickable: css`
+    cursor: pointer;
+    user-select: none;
+  `,
+  scrollableY: css`
+    overflow-x: hidden;
+    overflow-y: auto;
+    -webkit-overflow-scrolling: touch;
+  `,
+  customScrollbar: ({ width = 8, background = color.primary.inc4 } = {}) => css`
+    &::-webkit-scrollbar-track {
+      background-color: #ebecf0;
+    }
+
+    &::-webkit-scrollbar {
+      width: ${width}px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 100px;
+      background-color: ${background};
+    }
+  `,
+};
