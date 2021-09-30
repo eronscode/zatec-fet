7a8786fd05244babfa3c5ddea5bb217f511db52c
osemudiamen Elebesunu <eronscode@gmail.com>
2021-09-30 10:47:16 +0100

    feat: adds chevron icon

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -13,3 +13,17 @@ export const ErrorIcon = () => {
     </svg>
   );
 };
+
+export const ChevronIcon = () => {
+  return (
+    <svg
+      width='8'
+      height='4'
+      viewBox='0 0 8 4'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path d='M8 0H0L4 4L8 0Z' fill='#111111' />
+    </svg>
+  );
+};
